73b0c5570b59dca3541fd7a14c12c8944a39c444
SqlService_cp <SND\SqlService_cp>
2009-08-10 08:03:54 +0000

    Incremented version number after implementing UTF-8 support.

--- dev/csql/Properties/AssemblyInfo.cs
+++ dev/csql/Properties/AssemblyInfo.cs
@@ -36,6 +36,6 @@ using System;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion( "0.2.2.*" )]
-[assembly: AssemblyFileVersion( "0.2" )]
+[assembly: AssemblyVersion( "0.3.0.*" )]
+[assembly: AssemblyFileVersion( "0.3" )]
 [assembly: NeutralResourcesLanguageAttribute( "en-US" )]
