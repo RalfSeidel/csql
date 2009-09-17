757fa98e515865334b3ba03bc271f778404551ed
SqlService_cp <SND\SqlService_cp>
2009-09-17 09:05:30 +0000

    Interim checkin

---
+++ dev/csql/CsqlOptions.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace csql
+{
+    public class CsqlOptions
+    {
+        /// <summary>
+        /// Options for the pre processor.
+        /// </summary>
+        private SqtppOptions m_sqtppOptions;
+
+
+        private bool m_noLogo;
+
+        private int m_verbosity;
+
+
+        public string DistributionFile 
+
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="CsqlOptions"/> class.
+        /// </summary>
+        public CsqlOptions()
+        {
+        }
+    }
+}
---
+++ dev/csql/SqtppOptions.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace csql
+{
+    public class SqtppOptions
+    {
+        /// <summary>
+        /// Macro defintions passed to sqtpp with the /D argument
+        /// </summary>
+        private Dictionary<string, string> m_macroDefinitions = new Dictionary<string, string>();
+
+        /// <summary>
+        /// List of additional include directories
+        /// </summary>
+        private List<string> m_includeDirectories = new List<string>();
+
+        /// <summary>
+        /// Path of the main input file.
+        /// </summary>
+        private string m_inputFile;
+
+        /// <summary>
+        /// Path of the main output file.
+        /// </summary>
+        private string m_outputFile;
+
+        /// <summary>
+        /// Every other argument/option e.g. /E
+        /// </summary>
+        private string m_otherArguments = String.Empty;
+
+
+        public string CommandLineArguments
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
+    }
+}
